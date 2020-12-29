7a95d24eb9b6282d66072a12de4f97c43d6bf06d
raj99-developer <76556877+raj99-developer@users.noreply.github.com>
2020-12-29 14:38:02 +0530

    upload files

---
+++ MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.magicidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etID;
+    Button btnSubmit;
+    TextView tvResults;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etID=findViewById(R.id.etID);
+        btnSubmit=findViewById(R.id.btnSubmit);
+        tvResults=findViewById(R.id.tvResults);
+        tvResults.setVisibility(View.GONE);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String idNumber=etID.getText().toString().trim();
+                String dob=idNumber.substring(0,6);
+                int gender=Integer.parseInt(Character.toString(idNumber.charAt(6)));
+                String sGender;
+                if(gender<5)
+                    sGender=getString(R.string.female);
+                else
+                    sGender=getString(R.string.male);
+                int Nationality=Integer.parseInt(Character.toString(idNumber.charAt(10)));
+                String sNationality;
+                if(Nationality==0)
+                    sNationality=getString(R.string.sacit);
+                else
+                    sNationality=getString(R.string.perres);
+
+                String text=getString(R.string.dob)+ dob + "\n"+
+                        getString(R.string.gender)+ sGender+"\n"+
+                        getString(R.string.nationality)+ sNationality;
+                tvResults.setText(text);
+                tvResults.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
